c5d13d88824e3dac4b4fc9d6f1d420cbfced596d
megaplanet <megaplanet.github@megaplanet.org>
2015-07-17 19:58:30 +0200

    rename gendoc.bat -> .cmd + improve script

--- bin/gendoc.bat
+++
@@ -1,4 +0,0 @@
-cd docs
-rem make clean would be nice but exit from the script
-make.bat html
-rem cd .. would be nice but never reached
---
+++ bin/gendoc.cmd
@@ -0,0 +1,5 @@
+call bin\activate.cmd
+cd docs
+call make clean
+call make.bat html
+cd ..
