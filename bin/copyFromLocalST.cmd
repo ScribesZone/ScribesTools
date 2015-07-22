838068d3163637bcdc04fb9e9143f1137ffd37ff
megaplanet <megaplanet.github@megaplanet.org>
2015-07-23 00:28:47 +0200

    rename + misc

---
+++ bin/copyFromLocalST.cmd
@@ -0,0 +1,5 @@
+CALL res\checkstyle\copyFromLocalST.cmd
+CALL res\ganttproject\copyFromLocalST.cmd
+CALL res\kmade\copyFromLocalST.cmd
+CALL res\schemacrawler\copyFromLocalST.cmd
+CALL res\schemaspy\copyFromLocalST.cmd
--- bin/copyFromST.cmd
+++
@@ -1,5 +0,0 @@
-CALL res\checkstyle\copyFromST.cmd
-CALL res\ganttproject\copyFromST.cmd
-CALL res\kmade\copyFromST.cmd
-CALL res\schemacrawler\copyFromST.cmd
-CALL res\schemaspy\copyFromST.cmd
