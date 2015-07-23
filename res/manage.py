e2db062ffcd845abbc6e00a91465c38030d6297c
megaplanet <megaplanet.github@megaplanet.org>
2015-07-23 22:46:41 +0200

    add download and extract commands

--- res/manage.py
+++ res/manage.py
@@ -23,11 +23,11 @@ def parseArguments():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "command",
-        help="install or check",
+        help="install | check | download | extract",
         type=str)
     parser.add_argument(
-        "tool",
-        help="name of the tool",
+        "package",
+        help="name of the tool package (e.g. checkstyle)",
         type=str)
     arguments = parser.parse_args()
     return arguments
@@ -38,17 +38,21 @@ def displayContext():
     print('SRIBETOOLS directory  : %s' % SCRIBETOOLS)
     print('Platform              : %s' % PLATFORM)
 
-def processCommand(command, tool):
+def processCommand(command, toolPackage):
     try:
-        tool_module = importlib.import_module(tool.lower())
+        tool_module = importlib.import_module(toolPackage)
     except ImportError:
-        raise Exception('No tool named %s' % tool)
-    if command == 'install':
+        raise Exception('No tool named %s' % toolPackage)
+    if command == 'download':
+        tool_module.Tool().download()
+    elif command == 'install':
         tool_module.Tool().install()
     elif command == 'check':
         tool_module.Tool().check()
+    elif command == 'extract':
+        tool_module.Tool().extract()
     else:
         raise Exception('command is invalid')
 
 args = parseArguments()
-processCommand(args.command, args.tool)
+processCommand(args.command, args.package)
