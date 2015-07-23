e3129af1159278ad5413672f7e0caf4917cd871d
megaplanet <megaplanet.github@megaplanet.org>
2015-07-23 22:10:11 +0200

    refactor with framework

--- res/ganttproject/__init__.py
+++ res/ganttproject/__init__.py
@@ -1,12 +1,35 @@
 # coding=utf-8
 
-NAME='GanttProject'
-PACKAGE='ganttproject'
-BUNDLES={
-    '*':  'ganttproject-2.7-r1891.zip',
-}
+import shutil
 
-RESOURCES=[
-    'ganttproject.cmd',
-]
+import tools
+
+class Tool(tools.Tool):
+    name = 'GanttProject'
+    installPlatforms = ['*']
+    bundles = {
+        'ganttproject_zip' : {
+            '*':  ['ganttproject-2.7-r1891.zip']
+        }
+    }
+    locals=[
+        'ganttproject.cmd',
+    ]
+
+    def doInstall(self):
+        raise NotImplementedError()
+        # FIXME: to be implemented
+        self.unzipResourceToTargetThenRename('ganttproject_zip')
+
+    doCheck = tools.CmdsCheck(
+        message = 'Next step should open ganttproject help',
+        cmds = [ 'ganttproject.cmd -help' ],
+    )
+
+# FIXME: the tool reference must be automatically associated to the
+# attribute via a metaclass
+# TODO: implement the metaclass
+Tool.doCheck.tool = Tool
+
+
 
