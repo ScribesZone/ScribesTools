24246e83a019f799256aa8477de8e5ec08d72562
megaplanet <megaplanet.github@megaplanet.org>
2015-07-24 10:58:47 +0200

    new

---
+++ res/index.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from collections import OrderedDict
+
+ALL = OrderedDict({
+    'BOOT' : 'python',
+    'INSTALLERS' : 'java,github,graphviz,pandoc',
+    'BUNDLES' :
+        'checkstyle,ganttproject,kmade,schemacrawler,schemaspy'
+        ',sqlite,useocl',
+    'EXTRAS' : 'pycharm,modelio'
+})
