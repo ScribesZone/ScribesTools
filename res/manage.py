4a28fe9be7322ee6cf973481044b24cd531dcd0f
megaplanet <megaplanet.github@megaplanet.org>
2015-07-24 10:59:23 +0200

    add support for package groups

--- res/manage.py
+++ res/manage.py
@@ -17,17 +17,20 @@ import importlib
 import argparse
 
 from tools import ROOT_SOURCE_DIR, SCRIBETOOLS, PLATFORM
+import index
 
 
 def parseArguments():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "command",
-        help="install | check | download | extract",
+        "commands",
+        help="list of commands (install | check | download | extract)"
+            "separated by commas",
         type=str)
     parser.add_argument(
-        "package",
-        help="name of the tool package (e.g. checkstyle)",
+        "packages",
+        help="name(s) of the tool package(s) separated by commas"
+             "(e.g. checkstyle,sqlite,schemaspy )",
         type=str)
     arguments = parser.parse_args()
     return arguments
@@ -54,5 +57,31 @@ def processCommand(command, toolPackage):
     else:
         raise Exception('command is invalid')
 
-args = parseArguments()
-processCommand(args.command, args.package)
+def processCommands(commands, toolPackages):
+    for tool_package in toolPackages:
+        for command in commands:
+            processCommand(command, tool_package)
+
+def getPackageList(packages):
+    """ get a list a package name or groups and expand the groups """
+    package_list = []
+    for package in packages:
+        if package in index.ALL.keys():
+            print 'Group %s = %s' % (package,index.ALL[package])
+            # this is a group. Expand it
+            package_list.extend(index.ALL[package].split(','))
+        else:
+            # this is a reguar package
+            package_list.append(package)
+    print package_list
+    return package_list
+
+def main():
+    args = parseArguments()
+    command_list = args.commands.split(',')
+    package_list = getPackageList(args.packages.split(','))
+    processCommands(command_list, package_list)
+
+
+if __name__ == "__main__":
+    main()
