405a631cc5e8994452a37ae9cc683cdaedd16b10
megaplanet <megaplanet.github@megaplanet.org>
2015-07-24 10:54:21 +0200

    remove copyFromLocalST (replaced by manage.py extract)

--- bin/gendoc.cmd
+++ bin/gendoc.cmd
@@ -1,6 +1,6 @@
 @echo off
 call bin\activate.cmd
-call bin\copyFromLocalST.cmd
+REM FIXME:  go to res and python manage.py extract ALL or something like that
 cd docs
 call make clean
 call make.bat html
