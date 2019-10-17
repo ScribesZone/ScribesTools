41ddfdedd4d0ee4302af9a91b28d08615e5d06e4
escribis <escribis@users.noreply.github.com>
2019-10-17 13:09:30 +0200

    add a TODO file

---
+++ TODOs.py
@@ -0,0 +1,3 @@
+# TODO: Change the path to sphinx-build in Makefile
+# TODO: check why there is a bulk failure on Readthedocs
+# TODO: check why replace |L| cause errors on Readthedocs (see config.html)
