94f58629f1a6df5a5d600bdbc7dcc2414f02023e
escribis <escribis@users.noreply.github.com>
2019-12-10 09:58:41 +0100

    met à jour les TODOs

--- TODOs.py
+++ TODOs.py
@@ -1,3 +1,4 @@
 # TODO: Change the path to sphinx-build in Makefile
 # TODO: check why there is a bulk failure on Readthedocs
+#       USE OCL pages have been improved, but the result cannot be published
 # TODO: check why replace |L| cause errors on Readthedocs (see config.html)
